e2d805718378653ff10f5f4509fd5728ae16cf2c
HenryLeNgocThang <35796147+HenryLeNgocThang@users.noreply.github.com>
2022-11-30 21:37:18 +0100

    rev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material/material.module';
@@ -12,6 +12,7 @@ import { HeaderComponent } from './layout/header/header.component';
 
 // Pages (routing pages)
 import { QuizComponent } from './main/quiz/quiz.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,12 @@ import { QuizComponent } from './main/quiz/quiz.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [
