7949e7de78a35f964bfe1511c775ad1fc90884db
HenryLeNgocThang <35796147+HenryLeNgocThang@users.noreply.github.com>
2022-12-01 19:43:15 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+setTimeout(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+}, 300);
